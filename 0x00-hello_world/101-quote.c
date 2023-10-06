e72a4b58bb297553c5404901db1143a68a040e33
Malik <malikhussein229@gmail.com>
2023-10-06 12:51:13 -0700

    #

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,18 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
-* main= Entry point
-*
-* Return= 1 (Success)
-*/
+ * main - Entry point
+ *
+ * Description: a c program that uses unistd library
+ *
+ * Return: Always 1 (success)
+ */
+
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+
 	return (1);
 }
-
