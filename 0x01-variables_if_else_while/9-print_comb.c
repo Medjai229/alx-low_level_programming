4822b8fece0e50607098e51c845c0fd9167401fe
Malik <malikhussein229@gmail.com>
2023-10-08 18:24:31 -0700

    #

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: a c program thar prints single digit numbers
+ * with a space and ','
+ *
+ * Return: Always 0 (Success) 
+ */
+
+int main(void)
+{
+	int sd = 48;
+
+	while (sd < 58)
+	{
+		putchar(sd);
+		if (sd != 57)
+		{
+		putchar(44);
+		putchar(32);
+		}
+		sd++;
+	}
+	putchar('\n');
+
+	return (0);
+}
