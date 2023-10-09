f0417473c7b181516d134249d163e1392d3b19bf
Malik <malikhussein229@gmail.com>
2023-10-08 18:10:20 -0700

    #

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: a c program that prints base16 on order
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int b16 = 48;
+	while (b16 <= 103)
+	{
+		putchar(b16);
+		if (b16 == 57)
+		{
+			b16 += 39;
+		}
+
+		b16++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
