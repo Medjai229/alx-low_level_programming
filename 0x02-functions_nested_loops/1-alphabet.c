b1434e57da419514b972dc91ccb86a74baf3098a
Malik <malikhussein229@gmail.com>
2023-10-10 15:18:39 -0700

    #

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - a function that prints all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c >= 'z')
+	{
+		_putchar('c');
+		c++;
+	}
+
+	return (0);
+}
