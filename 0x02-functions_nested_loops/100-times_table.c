bd70d381dc3ff7598ecbf6f8787d04ea2556b1c4
Malik <malikhussein229@gmail.com>
2023-10-10 19:39:56 -0700

    #

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,8 +28,8 @@ void print_times_table(int n)
 					_putchar(' ');
 				if (k >= 100)
 				{
-					_putchar('0' + (k / 100);
-					_putchar('0' + (k / 10) % 10)
+					_putchar('0' + (k / 100));
+					_putchar('0' + (k / 10) % 10);
 				}
 				else if (k < 100 && k >= 10)
 					_putchar('0' + (k / 10));
