d54c5ba39340858e1f8184149080e5991db66b9b
Malik <malikhussein229@gmail.com>
2023-10-10 19:21:49 -0700

    #

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,9 +14,9 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
-				printf("%i, ", i);
+				printf("%d, ", i);
 			else if (i == 98)
-				printf("%i\n", i);
+				printf("%d\n", i);
 		}
 	}
 	else if (n > 98)
@@ -24,11 +24,11 @@ void print_to_98(int n)
 		for (j = n; j >= 98; j--)
 		{
 			if (j != 98)
-				printf("%i, ", i);
+				printf("%d, ", i);
 			else if (i == 98)
-				printf("%i\n", i);
+				printf("%d\n", i);
 		}
 	}
 	else if (n == 98)
-		printf("%i\n", n);
+		printf("%d\n", n);
 }
