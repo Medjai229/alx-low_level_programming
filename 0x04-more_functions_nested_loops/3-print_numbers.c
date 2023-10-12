053efb464eff48d18556da6fce017acb7ea78886
Malik <malikhussein229@gmail.com>
2023-10-12 09:30:59 -0700

    #

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,14 @@
 
 /**
  * print_numbers - prints the numbers from 0 to 9
+ * Return: void
  */
 
 void print_numbers(void)
 {
 	char i;
 
-	for (i = '0'; i <= 9; i++)
+	for (i = '0'; i <='9'; i++)
 	{
 		_putchar(c);
 	}
