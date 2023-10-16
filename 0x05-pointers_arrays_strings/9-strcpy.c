02425fd0f85de0fee28a9b52c24653dc922712ee
Malik <malikhussein229@gmail.com>
2023-10-16 15:17:20 -0700

    #

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,13 +5,14 @@
  * including the terminating null byte (\0), to the buffer pointed to by dest
  * @dest: the paste string
  * @src: the copy string
+ * Return: the value of dest
  */
 
 char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = -1; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
