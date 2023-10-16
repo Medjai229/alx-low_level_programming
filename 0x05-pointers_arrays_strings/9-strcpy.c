007e9b9b448aa89d8e1d299f394cfcfe2527bb4e
Malik <malikhussein229@gmail.com>
2023-10-16 15:20:38 -0700

    #

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,11 +10,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = 0;
 
-	for (i = -1; src[i] != '\0'; ++i)
+	do
 	{
 		dest[i] = src[i];
+		i++;
 	}
+	while (src[i] != '\0');
+
 	return (dest);
 }
