61123ce2e7432fa3596e7a10f217b770ed7f2756
Malik <malikhussein229@gmail.com>
2023-10-19 15:54:36 -0700

    #

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ *
+ * @c: input value
+ *
+ * Return: c
+ */
+
+char *leet(char *c)
+{
+	int i, j;
+	char s1[] = "AEOTL";
+	char s2[] = "43071";
+	
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (c[i] == s1[j] || c[i] == s1[j] + 32)
+			{
+				c[i] = s2[j];
+			}
+		}
+	}
+	return (c);
+}
+
