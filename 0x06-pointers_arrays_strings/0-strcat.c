60cf815f9a3cc33a24cb8b36cc85febdeec3271b
Malik <malikhussein229@gmail.com>
2023-10-19 14:46:46 -0700

    #

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ *
+ * */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+
+	while (dest[i])
+	{
+		c++;
+	}
+
+	for (j = 0; src[j]; j++)
+	{
+		dest[i++] = src[j]
+	}
+
+	return (dest);
+}
