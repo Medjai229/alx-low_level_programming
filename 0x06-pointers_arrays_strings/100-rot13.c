247fde930f986d50ed8b36c22d5f0dd776dde4e1
Malik <malikhussein229@gmail.com>
2023-10-19 16:03:06 -0700

    #

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ *
+ * @c: input value
+ *
+ * Return: c
+ */
+
+char *rot13(char *c)
+{
+	int i, j;
+	char normal[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char r13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (c[i] == normal[j])
+			{
+				c[i] = r13[j];
+			}
+		}
+	}
+	return (c);
+}
+
