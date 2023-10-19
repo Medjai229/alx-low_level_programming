20e058105c42aee2774ba3ef5f66886634f5e194
Malik <malikhussein229@gmail.com>
2023-10-19 15:28:25 -0700

    #

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * string_toupper - hanges all lowercase letters of a string to uppercase
+ *
+ * @str: the string
+ *
+ * Return: str
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
