33f698897d6b775cf705bb4063c307f310144fa4
Malik <malikhussein229@gmail.com>
2023-10-31 07:25:23 -0700

    #

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,7 +22,7 @@ char *str_concat(char *s1, char *s2)
 	while (s2[j] != '\0')
 		j++;
 
-	conc = malloc(sizeof(char) * (i + j + 2));
+	conc = malloc(sizeof(char) * (i + j + 1));
 
 	if (conc == NULL)
 		return (NULL);
