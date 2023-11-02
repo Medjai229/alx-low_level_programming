d37453654ff4aeacf411157192ab380e6a0534a1
Medjai229 <115402766+Medjai229@users.noreply.github.com>
2023-11-03 00:05:42 +0200

    #

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -19,65 +19,68 @@ int _isdigit(int c)
  *
  * Return: integer length of string
  */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (*s++)
-		i++;
-	return (i);
+int _strlen(char *str) {
+    int i = 0;
+    while (str[i] != '\0') {
+        i++;
+    }
+    return i;
 }
 
 /**
- * big_multiply - multiply two big number strings
- * @s1: the first big number string
- * @s2: the second big number string
+ * multiply - multiply two big number strings
+ * @str1: the first big number string
+ * @str2: the second big number string
  *
  * Return: the product big number string
  */
-char *big_multiply(char *s1, char *s2)
-{
-	char *r;
-	int l1, l2, a, b, c, x;
+char *multiply(char *str1, char *str2) {
+    int l1 = _strlen(str1);
+    int l2 = _strlen(str2);
+    char *res = malloc(l1 + l2 + 1);
 
-	l1 = _strlen(s1);
-	l2 = _strlen(s2);
-	r = malloc(a = x = l1 + l2);
-	if (!r)
-		printf("Error\n"), exit(98);
-	while (a--)
-		r[a] = 0;
+    if (res == NULL) {
+        printf("Error: Memory allocation failed\n");
+        exit(1);
+    }
 
-	for (l1--; l1 >= 0; l1--)
-	{
-		if (!_isdigit(s1[l1]))
-		{
-			free(r);
-			printf("Error\n"), exit(98);
-		}
-		a = s1[l1] - '0';
-		c = 0;
+    for (int i = 0; i < l1 + l2; i++) {
+        res[i] = '0';
+    }
+    res[l1 + l2] = '\0';
 
-		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
-		{
-			if (!_isdigit(s2[l2]))
-			{
-				free(r);
-				printf("Error\n"), exit(98);
-			}
-			b = s2[l2] - '0';
+    for (int i = l1 - 1; i >= 0; i--) {
+        if (!_isdigit(str1[i])) {
+            free(res);
+            printf("Error: Invalid character in the first number\n");
+            exit(1);
+        }
 
-			c += r[l1 + l2 + 1] + (a * b);
-			r[l1 + l2 + 1] = c % 10;
+        int a = str1[i] - '0';
+        int carry = 0;
 
-			c /= 10;
-		}
-		if (c)
-			r[l1 + l2 + 1] += c;
-	}
-	return (r);
+        for (int j = l2 - 1; j >= 0; j--) {
+            if (!_isdigit(str2[j])) {
+                free(res);
+                printf("Error: Invalid character in the second number\n");
+                exit(1);
+            }
+
+            int b = str2[j] - '0';
+            int product = (res[i + j + 1] - '0') + a * b + carry;
+            res[i + j + 1] = (product % 10) + '0';
+            carry = product / 10;
+        }
+
+        res[i] += carry;
+    }
+
+    while (*res == '0' && *(res + 1) != '\0') {
+        res++;
+    }
+
+    return res;
 }
-
 
 /**
  * main - multiply two big number strings
@@ -88,27 +91,31 @@ char *big_multiply(char *s1, char *s2)
  */
 int main(int argc, char **argv)
 {
-	char *r;
-	int a, c, x;
+	char *res;
+	int i = 0, cd, tl;
 
 	if (argc != 3)
-		printf("Error\n"), exit(98);
-
-	x = _strlen(argv[1]) + _strlen(argv[2]);
-	r = big_multiply(argv[1], argv[2]);
-	c = 0;
-	a = 0;
-	while (c < x)
 	{
-		if (r[c])
-			a = 1;
-		if (a)
-			_putchar(r[c] + '0');
-		c++;
+		printf("Error\n");
+		exit(98);
 	}
-	if (!a)
+
+	res = multi(argv[1], argv[2]);
+	tl = _strlen(res);
+	cd = 0;
+
+	while (cd < tl)
+	{
+		if (res[cd])
+			i = 1;
+
+		if (i)
+			_putchar(res[cd] + '0');
+		cd++;
+	}
+	if (!i)
 		_putchar('0');
 	_putchar('\n');
-	free(r);
+	free(res);
 	return (0);
 }
