6a8b32a949bb94c9d345e4030c0367947926d752
Malik <malikhussein229@gmail.com>
2023-11-02 14:34:02 -0700

    #

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,20 +5,20 @@
 /**
  * _isdigit - checks if character is digit
  *
- * @c: the character to check
+ * @ch: the character to check
  *
  * Return: 1 if digit otherwise 0
  */
 
 int _isdigit(int ch)
 {
-	return (c >= '0' && c <= '9');
+	return (ch >= '0' && ch <= '9');
 }
 
 /**
  * _strlen - finds the lenght of a string
  *
- * @s: the string to find its lenght
+ * @str: the string to find its lenght
  *
  * Return: integer contains the lenght of the string
  */
@@ -34,7 +34,12 @@ int _strlen(char *str)
 }
 
 /**
+ * multi - multiply two strings
  *
+ * @str1: the first string
+ * @str2: the second string
+ *
+ * Return: the product of the two string multiplied
  */
 
 char *multi(char *str1, char *str2)
@@ -44,17 +49,14 @@ char *multi(char *str1, char *str2)
 
 	l1 = _strlen(str1);
 	l2 = _strlen(str2);
-
 	res = malloc(l1 + l2);
 	if (res == NULL)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
 	for (i = 0; i < l1 + l2; i++)
 		res[i] = '0';
-
 	for (i = l1 - 1; i >= 0; i--)
 	{
 		if (!_isdigit(str1[i]))
@@ -64,7 +66,6 @@ char *multi(char *str1, char *str2)
 			exit(98);
 		}
 		a = str1[i] - '0';
-
 		for (j = l2 - 1; j >= 0; j--)
 		{
 			if (!_isdigit(str2[j]))
@@ -75,15 +76,52 @@ char *multi(char *str1, char *str2)
 			}
 			b = str2[j] - '0';
 			pro = (res[i + j + 1] - '0') + a * b + car;
-			res[i + j + 1] = (pro & 10) + '0';
+			res[i + j + 1] = (pro % 10) + '0';
 			car = pro / 10;
 		}
-
 		res[i] += car;
 	}
-
 	while (*res == '0' && *(res + 1) != '\0')
 		res++;
-
 	return (res);
+}
+
+/**
+ * main - multiply two numbers
+ *
+ * @argc: number of arg
+ * @argv: array of arg
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	char *res;
+	int i = 0, cd, tl;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	res = multi(argv[1], argv[2]);
+	tl = _strlen(res);
+	cd = 0;
+
+	while (cd < tl)
+	{
+		if (res[cd])
+			i = 1;
+
+		if (i)
+			_putchar(res[cd] + '0');
+		cd++;
+	}
+	if (!i)
+		_putchar('0');
+	_putchar('\n');
+	free(res);
+	return (0);
 }
