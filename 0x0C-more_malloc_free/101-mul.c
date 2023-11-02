a71b78b617c5fdecb546b3eaa9533384d5cb8ca4
Malik <malikhussein229@gmail.com>
2023-11-02 14:13:57 -0700

    #

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,92 +1,89 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
 
 /**
- * _puts - prints a string
+ * _isdigit - checks if character is digit
  *
- * @str: the string to be printed
+ * @c: the character to check
+ *
+ * Return: 1 if digit otherwise 0
  */
 
-void _puts(const char *str)
+int _isdigit(int ch)
+{
+	return (c >= '0' && c <= '9');
+}
+
+/**
+ * _strlen - finds the lenght of a string
+ *
+ * @s: the string to find its lenght
+ *
+ * Return: integer contains the lenght of the string
+ */
+
+int _strlen(char *str)
 {
 	int i = 0;
 
-	while (str[i])
-	{
-		_putchar(str[i]);
+	while (*str++)
 		i++;
-	}
+
+	return (i);
 }
 
 /**
- * print_int - print an int
  *
- * @n: the int to be printed
  */
 
-void print_int(unsigned long int n)
+char *multi(char *str1, char *str2)
 {
-	unsigned long int div = 1, i, res;
+	int l1, l2, i, j, a, b, pro, car = 0;
+	char *res;
 
-	for (i = 0; n / div > 9; i++, div *= 10)
-		;
+	l1 = _strlen(str1);
+	l2 = _strlen(str2);
 
-
-	for (; div >= 1; n %= div, div /= 10)
+	res = malloc(l1 + l2);
+	if (res == NULL)
 	{
-		res = n / div;
-		_putchar('0' + res);
-	}
-}
-
-/**
- * _atoi - convert a strig to an integer
- *
- * @str: the string to be converted
- *
- * Return: an integer
- */
-
-int _atoi(const char *str)
-{
-	int sign = 1;
-	unsigned long int res = 0, fn, i;
-
-	for (fn = 0; !(str[fn] >= '0' && str[fn] <= '9'); fn++)
-	{
-		if (str[fn] == '-')
-			sign *= -1;
-	}
-
-	for (i = fn; str[i] >= '0' && str[i] <= '9'; i++)
-	{
-		res = res * 10 + (str[i] - '0');
-	}
-	return (sign * res);
-}
-
-/**
- * main - multiplies two positive numbers
- *
- * @argc: number of arg
- * @argv: array of arg
- *
- * Return: Always 0
- */
-
-int main(int argc, char *argv[])
-{
-	(void)argc;
-
-	if (argc != 3)
-	{
-		_puts("Error ");
+		printf("Error\n");
 		exit(98);
 	}
 
-	print_int(_atoi(argv[1]) * _atoi(argv[2]));
-	_putchar('\n');
+	for (i = 0; i < l1 + l2; i++)
+		res[i] = '0';
 
-	return (0);
+	for (i = l1 - 1; i >= 0; i--)
+	{
+		if (!_isdigit(str1[i]))
+		{
+			free(res);
+			printf("Error\n");
+			exit(98);
+		}
+		a = str1[i] - '0';
+
+		for (j = l2 - 1; j >= 0; j--)
+		{
+			if (!_isdigit(str2[j]))
+			{
+				free(res);
+				printf("Error\n");
+				exit(98);
+			}
+			b = str2[j] - '0';
+			pro = (res[i + j + 1] - '0') + a * b + car;
+			res[i + j + 1] = (pro & 10) + '0';
+			car = pro / 10;
+		}
+
+		res[i] += car;
+	}
+
+	while (*res == '0' && *(res + 1) != '\0')
+		res++;
+
+	return (res);
 }
