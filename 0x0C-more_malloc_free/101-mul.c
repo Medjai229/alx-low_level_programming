692bfb6f09fa56985610e627123ac07239f2b24e
Malik <malikhussein229@gmail.com>
2023-11-02 15:36:39 -0700

    #

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,109 +1,119 @@
 #include "main.h"
-#include <stdio.h>
 #include <string.h>
 
 /**
  * _isdigit - checks if character is digit
- * @c: the character to check
+ *
+ * @ch: the character to check
  *
  * Return: 1 if digit, 0 otherwise
  */
 
-int _isdigit(int ch) {
-    return (ch >= '0' && ch <= '9');
+int _isdigit(int ch)
+{
+	return (ch >= '0' && ch <= '9');
 }
 
 /**
  * _strlen - returns the length of a string
- * @s: the string whose length to check
+ *
+ * @str: the string whose length to check
  *
  * Return: integer length of string
  */
-int _strlen(char *str) {
-    int i = 0;
-    while (str[i] != '\0') {
-        i++;
-    }
-    return i;
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
 }
 
 /**
  * multiply - multiply two big number strings
- * @str1: the first big number string
- * @str2: the second big number string
+ *
+ * @str1: the first number string
+ * @str2: the second number string
  *
  * Return: the product big number string
  */
-char *multiply(char *str1, char *str2) {
-    int l1 = _strlen(str1);
-    int l2 = _strlen(str2);
-    char *res = malloc(l1 + l2 + 1);
 
-    if (res == NULL) {
-        printf("Error\n");
-        exit(98);
-    }
+char *multiply(char *str1, char *str2)
+{
+	int l1, l2, i, j, a, b, carry, product;
+	char *res;
 
-    for (int i = 0; i < l1 + l2; i++) {
-        res[i] = '0';
-    }
-    res[l1 + l2] = '\0';
+	l1 = _strlen(str1), l2 = _strlen(str1);
+	res = malloc(l1 + l2 + 1);
+	if (res == NULL)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 0; i < l1 + l2; i++)
+		res[i] = '0';
+	res[l1 + l2] = '\0';
+	for (i = l1 - 1; i >= 0; i--)
+	{
+		if (!_isdigit(str1[i]))
+		{
+			free(res);
+			printf("Error\n");
+			exit(98);
+		}
+		a = str1[i] - '0';
+		carry = 0;
 
-    for (int i = l1 - 1; i >= 0; i--) {
-        if (!_isdigit(str1[i])) {
-            free(res);
-            printf("Error\n");
-            exit(98);
-        }
+		for (j = l2 - 1; j >= 0; j--)
+		{
+			if (!_isdigit(str2[j]))
+			{
+				free(res);
+				printf("Error\n");
+				exit(98);
+			}
+			b = str2[j] - '0';
+			product = (res[i + j + 1] - '0') + a * b + carry;
 
-        int a = str1[i] - '0';
-        int carry = 0;
-
-        for (int j = l2 - 1; j >= 0; j--) {
-            if (!_isdigit(str2[j])) {
-                free(res);
-                printf("Error\n");
-                exit(98);
-            }
-
-            int b = str2[j] - '0';
-            int product = (res[i + j + 1] - '0') + a * b + carry;
-            res[i + j + 1] = (product % 10) + '0';
-            carry = product / 10;
-        }
-
-        res[i] += carry;
-    }
-
-    while (*res == '0' && *(res + 1) != '\0') {
-        res++;
-    }
-
-    return res;
+			res[i + j + 1] = (product % 10) + '0';
+			carry = product / 10;
+		}
+		res[i] += carry;
+	}
+	while (*res == '0' && *(res + 1) != '\0')
+		res++;
+	return (res);
 }
 
 /**
  * main - multiply two big number strings
+ *
  * @argc: the number of arguments
  * @argv: the argument vector
  *
  * Return: Always 0 on success.
  */
-int main(int argc, char **argv) {
-    if (argc != 3) {
-        printf("Error: Usage: %s <number1> <number2>\n", argv[0]);
-        return 1;
-    }
 
-    char *result = multiply(argv[1], argv[2]);
+int main(int argc, char **argv)
+{
+	char *result;
 
-    if (*result == '\0') {
-        putchar('0');
-    } else {
-        printf("%s\n", result);
-    }
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    free(result);
+	result = multiply(argv[1], argv[2]);
 
-    return 0;
+	if (*result == '\0')
+		_putchar('0');
+	else
+		printf("%s\n", result);
+
+	free(result);
+
+	return (0);
 }
