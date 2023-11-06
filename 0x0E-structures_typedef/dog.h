883965ecd2f863ba3c6eedaa3a74d152913b9cda
Malik <malikhussein229@gmail.com>
2023-11-06 14:59:02 -0800

    #

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,6 +9,8 @@
  * @owner: name of the owner
  */
 
+#typedef struct dog dog_t
+
 struct dog
 {
 	char *name;
@@ -17,5 +19,6 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif /*DOG_H*/
