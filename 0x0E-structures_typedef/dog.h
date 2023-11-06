0bc072e1dc6c4321cc34369a36220583d7555e70
Malik <malikhussein229@gmail.com>
2023-11-06 14:21:01 -0800

    #

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dog datatype
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ */
+
 struct dog
 {
 	char *name;
