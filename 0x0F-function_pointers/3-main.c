277c70a2d223155133844621523695b2df80a234
Malik <malikhussein229@gmail.com>
2023-11-08 12:22:41 -0800

    #

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,14 +14,15 @@ int main(int argc, char *argv[])
 	int result;
 	int (*func)(int, int);
 
+	func = get_op_func(argv[2]);
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*'
-	&& argv[2][0] != '/' && argv[2][0] != '%') || (argv[2][1] != '\0'))
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
