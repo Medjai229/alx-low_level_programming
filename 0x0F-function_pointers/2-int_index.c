e9705f4cc2f11c2881ff9092898104b2fc4d060e
Malik <malikhussein229@gmail.com>
2023-11-08 11:11:00 -0800

    #

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * 
+ * @array: array of intergers
+ * @size: size of array
+ * @cmp: ponter to function
+ * 
+ * Return: if any number match return its index of not return -1 
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && size && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
+		}
+	}
+	return (-1);
+}
