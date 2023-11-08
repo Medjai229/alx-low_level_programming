879ece31225092680f0f10f4f64cc0e71ce81a39
Malik <malikhussein229@gmail.com>
2023-11-08 13:27:06 -0800

    #

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,24 +2,23 @@
 
 /**
  * get_op_func - pointer to a function that performs an operation
- *
+ * 
  * @s: the operation
- *
+ * 
  * Return: a function that operates with two integers
  */
 
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-
-	int i;
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
 
 	for (i = 0; (ops[i].op[0] != s[0]) && i < 5; i++)
 	{
