b11ba67704affabd5ebde01f822d9c2aacf47ed1
Malik <malikhussein229@gmail.com>
2023-11-09 16:07:45 -0800

    #

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,98 @@
+#include "variadic_functions.h"
+
+/**
+ * printc - prints a character
+ *
+ * @arg: arguments to be printed
+ */
+
+void printc(va_list arg)
+{
+	char c;
+
+	c = va_arg(arg, int);
+	printf("%c", c);
+}
+
+/**
+ * printi - prints an integer
+ *
+ * @arg: arguments to be printed
+ */
+
+void printi(va_list arg)
+{
+	int i;
+
+	i = va_arg(arg, int);
+	printf("%d", i);
+}
+
+/**
+ * printfl - prints a float
+ *
+ * @arg: arguments to be printed
+ */
+
+void printfl(va_list arg)
+{
+	float f;
+
+	f = va_arg(arg, double);
+	printf("%f", f);
+}
+
+/**
+ * prints - prints a string
+ *
+ * @arg: arguments to be printed
+ */
+
+void prints(va_list arg)
+{
+	char *s;
+
+	s = va_arg(arg, char *);
+
+	if (s != NULL)
+		printf("%s", s);
+	else
+		printf("(nil)");
+}
+
+/**
+ * print_all - prints anything
+ *
+ * @format: string contains symobls of the data types
+ * @...: the arguments to be printed
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list all;
+	int i = 0, j;
+	char *sep = "";
+	prt_t funcs[] = {{"c", printc}, {"i", printi},
+	{"f", printfl}, {"s", prints}};
+
+	va_start(all, format);
+
+	while (format && (*(format + i)))
+	{
+		j = 0;
+
+		while (j < 4 && *(format + i) != *(funcs[j].sym))
+			j++;
+
+		if (j < 4)
+		{
+			printf("%s", sep);
+			funcs[j].func(all);
+			sep = ", ";
+		}
+		i++;
+	}
+	printf("\n");
+
+	va_end(all);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,19 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * struct prt - a new type that defines a print
+ *
+ * @sym: the symbol that specifiy data type
+ * @func: pointer to a function that prints argument according to the sym
+ */
+
+typedef struct prt
+{
+	char *sym;
+	void (*func)(va_list arg);
+} prt_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
