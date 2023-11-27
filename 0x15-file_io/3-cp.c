2542cc1cd4a693f6e0ec9f6a99356cbb4ded9758
Medjai229 <115402766+Medjai229@users.noreply.github.com>
2023-11-28 00:16:37 +0200

    #

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,34 +4,35 @@
 #define ERR_NOREAD "Error: Can't read from file %s\n"
 #define ERR_NOWRITE "Error: Can't write to %s\n"
 #define ERR_NOCLOSE "Error: Can't close fd %d\n"
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
 /**
  * main - program
- * @argc: argument count
- * @argv: argument vector
+ * @ac: argument count
+ * @av: argument vector
  *
  * Return: 1 on success 0 on failure
  */
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
 	int from_fd = 0, to_fd = 0;
 	ssize_t b;
-	char buf[BUF_SIZE_READ];
+	char buf[READ_BUF_SIZE];
 
-	if (argc != 3)
+	if (ac != 3)
 		dprintf(STDERR_FILENO, USAGE), exit(97);
-	from_fd = open(argv[1], O_RDONLY);
+	from_fd = open(av[1], O_RDONLY);
 	if (from_fd == -1)
-		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
-	to_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (to_fd == -1)
-		dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
+		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 
-	while ((b = read(from_fd, buf, BUF_SIZE_READ)) > 0)
+	while ((b = read(from_fd, buf, READ_BUF_SIZE)) > 0)
 		if (write(to_fd, buf, b) != b)
-			dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
+			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 	if (b == -1)
-		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
 
 	from_fd = close(from_fd);
 	to_fd = close(to_fd);
