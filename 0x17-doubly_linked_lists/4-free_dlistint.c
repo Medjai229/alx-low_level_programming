fc02e5e18be67d296ad13e84cefa1b670c52f18a
Malik <malikhussein229@gmail.com>
2024-01-11 16:42:46 -0800

    #

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * free_flistint - frees a dlistint_t list
+ * free_dlistint - frees a dlistint_t list
  * @head: address of the head node
  */
 void free_dlistint(dlistint_t *head)
