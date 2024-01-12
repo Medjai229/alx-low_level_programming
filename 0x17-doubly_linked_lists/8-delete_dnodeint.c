4d81024c1529a979e6c5a9292910bfc5d448d98a
Malik <malikhussein229@gmail.com>
2024-01-11 17:33:24 -0800

    #

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -29,7 +29,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		temp->prev->next = temp->next
+		temp->prev->next = temp->next;
 			if (temp->next != NULL)
 				temp->next->prev = temp->prev;
 	}
