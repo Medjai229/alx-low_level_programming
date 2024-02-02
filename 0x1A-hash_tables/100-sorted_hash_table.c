00cbad0deff2c7b8bf43bc4198b94d03f25a77bd
Malik <malikhussein229@gmail.com>
2024-02-02 07:26:54 -0800

    #

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -142,5 +142,81 @@ char *shash_table_get(const shash_table_t *ht, const char *key)
 }
 
 /**
- *
+ * shash_table_print - prints a sorted hash table
+ * @ht: the sorted hash table
  */
+void shash_table_print(const shash_table_t *ht)
+{
+	shash_node_t *traverse;
+	int flag = 0;
+
+	if (ht == NULL)
+		return;
+
+	traverse = ht->shead;
+
+	printf("{");
+
+	while (traverse != NULL)
+	{
+		if (flag == 1)
+			printf(", ");
+		printf("'%s': '%s'", traverse->key, traverse->value);
+		flag = 1;
+		traverse = traverse->snext;
+	}
+	printf("}\n");
+}
+
+/**
+ * shash_table_print_rev - prints a sorted hash table in reverse
+ * @ht: the sorted hash table
+ */
+void shash_table_print_rev(const shash_table_t *ht)
+{
+	shash_node_t *traverse;
+	int flag = 0;
+
+	if (ht == NULL)
+		return;
+
+	traverse = ht->stail;
+
+	printf("{");
+
+	while (traverse != NULL)
+	{
+		if (flag == 1)
+			printf(", ");
+		printf("'%s': '%s'", traverse->key, traverse->value);
+		flag = 1;
+		traverse = traverse->sprev;
+	}
+	printf("}\n");
+}
+
+/**
+ * shash_table_delete - deletes a sorted hash table
+ * @ht: the sorted hash table
+ */
+void shash_table_delete(shash_table_t *ht)
+{
+	shash_node_t *traverse, *tmp;
+
+	if (ht == NULL)
+		return;
+
+	traverse = ht->shead;
+
+	while (traverse != NULL)
+	{
+		tmp = traverse->snext;
+		free(traverse->key);
+		free(traverse->value);
+		free(traverse);
+		traverse = tmp;
+	}
+
+	free(ht->array);
+	free(ht);
+}
