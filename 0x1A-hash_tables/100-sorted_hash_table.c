cc76f87db519d01ebdb4b72e061e763d71f8d490
Malik <malikhussein229@gmail.com>
2024-02-02 07:01:32 -0800

    #

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -29,3 +29,118 @@ shash_table_t *shash_table_create(unsigned long int size)
 
 	return (new_table);
 }
+
+/**
+ * shash_table_set - adds an element to the sorted hash table
+ * @ht: the sorted hash table to add or update the key/value
+ * @key: the key
+ * @value: the value associated with the key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int shash_table_set(shash_table_t *ht, const char *key, const char *value)
+{
+	shash_node_t *new_node, *traverse;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == 0 || value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	traverse = ht->shead;
+
+	while (traverse != NULL)
+	{
+		if (strcmp(traverse->key, key) == 0)
+		{
+			free(traverse->value);
+			traverse->value = strdup(value);
+			return (1);
+		}
+		traverse = traverse->snext;
+	}
+
+	new_node = malloc(sizeof(shash_node_t));
+
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	if (ht->shead == NULL)
+	{
+		new_node->snext = NULL;
+		new_node->sprev = NULL;
+		ht->shead = new_node;
+	}
+	else if (strcmp(ht->shead->key, key) > 0)
+	{
+		new_node->snext = ht->shead;
+		new_node->sprev = NULL;
+		ht->shead->sprev = new_node;
+		ht->shead = new_node;
+	}
+	else
+	{
+		traverse = ht->shead;
+		while (traverse->snext != NULL && strcmp(traverse->key, key) < 0)
+			traverse = traverse->snext;
+
+		new_node->snext = traverse->snext;
+		new_node->sprev = traverse;
+
+		if (traverse->snext == NULL)
+			ht->stail = new_node;
+		else
+			traverse->snext->sprev = new_node;
+		traverse->snext = new_node;
+	}
+
+	return (1);
+}
+
+/**
+ * shash_table_get - retrieves a value associated with a key
+ * @ht: the sorted hash table to look into
+ * @key: the key to look for
+ *
+ * Return: the value associated with the element
+ * or NULL if key couldn’t be found
+ */
+char *shash_table_get(const shash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	shash_node_t *node;
+
+	if (ht == NULL || key == NULL || *key == 0)
+		return (NULL);
+
+	index = key_index((const unsigned char *) key, ht->size);
+
+	if (index >= ht->size)
+		return (NULL);
+
+	node ht->shead;
+
+	while (node && strcmp(node->key, key) != 0)
+		node = node->snext;
+
+	if (node == NULL)
+		return (NULL);
+
+	return (node->value);
+}
+
+/**
+ *
+ */
